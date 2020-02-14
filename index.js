84be2bec4da00eddf055bf36889733ba317d6086
loyal-transpiler-0513 <ifeawoyale@yahoo.com>
2020-02-14 05:56:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.pop()
+  return kittens.slice(1)
 
 }
 function removeFirstKitten() {
-  return kittens.slice(1)
+  return kittens.slice(0, kittens.length -1)
 
 }
